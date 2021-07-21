2aed6abea3383ee362bcd79b66a836632199d039
Katawa <55095626+aasaringyulyan@users.noreply.github.com>
2021-07-21 14:34:44 +0300

    add func

--- functions/inputOutputParam/main.go
+++ functions/inputOutputParam/main.go
@@ -1,1 +1,17 @@
-package inputOutputParam
+package main
+
+import "fmt"
+
+func main() {
+	var s1 = "A"
+	var s2 = "B"
+
+	swapStrings(&s1, &s2)
+
+	fmt.Println("s1 =", s1)
+	fmt.Println("s2 =", s2)
+}
+
+func swapStrings(s1, s2 *string) {
+	*s1, *s2 = *s2, *s1
+}
